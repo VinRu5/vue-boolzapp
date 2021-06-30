8698dc29ff58ce4315b9ea6cca8daaa65f31250d
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-06-30 12:24:14 +0200

    inizio css

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ new Vue(
                 },
                 {
                     name: 'Luisa',
-                    avatar: '_4',
+                    avatar: '_6',
                     visible: true,
                     messages: [
                         {
