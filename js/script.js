d6deecea8c2cac9d5330ad78a8432c8166b03036
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-02 11:05:54 +0200

    milestone 4

--- js/script.js
+++ js/script.js
@@ -88,6 +88,89 @@ new Vue(
                         }
                     ],
                 },
+                {
+                    name: 'Marco',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Filippo',
+                    avatar: '_5',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Martina',
+                    avatar: '_io',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Daniele',
+                    avatar: '_7',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
             ],
             contactIndex: 0,
             textMessage: '',
@@ -139,18 +222,8 @@ new Vue(
             },
             searchContact: function() {
                 return this.contacts.filter((contact) => {
-                    if (this.search === '') {
+                    if (contact.name.includes(this.search)) {
                         return true;
-                    };
-                      
-                    for (let i = 0; i < this.search.length; i++) {
-                        debugger;
-                        for (let x = 0; x < contact.name.length; x++) {
-    
-                            if (this.search[i] === contact.name[x]) {
-                                return true;
-                            }
-                        }
                     }
                 });
             }
